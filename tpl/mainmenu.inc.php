6d4216760e1c86de15fe2a740277df9cab0fb869
Blacker <oleg_frolov@mail.ru>
2020-11-06 11:45:42 +0300

    update

--- tpl/mainmenu.inc.php
+++ tpl/mainmenu.inc.php
@@ -9,7 +9,47 @@
         <span></span>
         <span></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <wb-data wb="table=pages&item=mainmenu&field=prop">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto">
+                <wb-foreach wb-from="data">
+                    <li class="nav-item" wb-if='"{{count({{children}})}}" == 0'>
+                        <a class="nav-link" href="{{data.link}}" role="button">{{name}}</a>
+                    </li>
+                    <li class="nav-item dropdown" wb-if='"{{count({{children}})}}" > 0'>
+                        <a class="nav-link dropdown-toggle" href="{{data.link}}" role="button" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">{{name}}</a>
+                        <ul class="dropdown-menu shadow">
+                            <wb-foreach wb-from="children">
+                                <li wb-if='"{{count({{children}})}}" == 0'>
+                                    <a class="dropdown-item" href="{{data.link}}">{{name}}</a>
+                                </li>
+                                <li class="dropdown" wb-if='"{{count({{children}})}}" > 0'>
+                                    <a class="dropdown-item" href="{{data.link}}">{{name}}</a>
+                                    <ul class="dropdown-menu shadow">
+                                    <wb-foreach wb-from="children">
+                                        <li>
+                                        <a class="dropdown-item" href="property-thumbnail-full-width.html">
+                                        {{name}}
+                                        </a></li>
+                                        </wb-foreach>
+                                    </ul>
+                                </li>
+                            </wb-foreach>
+                        </ul>
+
+                        </a>
+                    </li>
+                </wb-foreach>
+            </ul>
+            <ul class="admin-info color-white-a">
+                <li><a href="/cms/signin">Вход</a></li>
+                <li><a href="/cms/signup">Регистрация</a></li>
+            </ul>
+        </div>
+    </wb-data>
+</nav>
+<!--
         <ul class="navbar-nav mx-auto">
             <li class="nav-item">
                 <a class="nav-link" href="/" role="button">Главная</a>
@@ -128,11 +168,6 @@
                 </form>
             </li>
         </ul>
-        <ul class="admin-info color-white-a">
-            <li><a href="/cms/signin">Вход</a></li>
-            <li><a href="/cms/signup">Регистрация</a></li>
-        </ul>
-    </div>
-</nav>
+    -->
 
 </html>
