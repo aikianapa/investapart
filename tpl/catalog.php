e5e542a0241d5c6a1e540cc548050e6e31652aa0
Blacker <oleg_frolov@mail.ru>
2020-11-19 01:52:23 +0300

    update

--- tpl/catalog.php
+++ tpl/catalog.php
@@ -109,7 +109,7 @@
                                     <div class="col-md-12 col-lg-6 col-xl-4">
                                         <div class="property-thumbnail mt-30">
                                             <div class="property-img position-relative overflow-hidden overlay-secondary-4">
-                                                <img src="/thumbc/725x635/src/{{images.0.img}}" alt="name">
+                                                <img src="/thumbc/555x485/src/{{images.0.img}}" alt="name">
                                                 <div class="thumbnail-content z-index-1 color-white-a color-white">
                                                     <span class="thum-category category-1 bg-secondary color-white z-index-1 px-15">For Sale
                                                     </span>
@@ -212,7 +212,7 @@
                                     <div class="col-md-12 col-lg-6 col-xl-4">
                                         <div class="property-thumbnail mt-30">
                                             <div class="property-img position-relative overflow-hidden overlay-secondary-4">
-                                                <img src="/thumbc/725x635/src/{{images.0.img}}" alt="name">
+                                                <img src="/thumbc/555x485/src/{{images.0.img}}" alt="name">
                                                 <div class="thumbnail-content z-index-1 color-white-a color-white">
                                                     <span class="thum-category category-1 bg-secondary color-white z-index-1 px-15">
                                                         {{square}} кв.м.
